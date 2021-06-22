5333af5d9399e564aaff424b2f5fa3831f0d0a01
Guillermo Ampie <ga.ampie@gmail.com>
2021-06-22 18:21:17 -0500

    Reformat app.py and add port 8080

--- app.py
+++ app.py
@@ -4,27 +4,34 @@ import logging
 
 app = Flask(__name__)
 
+
 @app.route('/status')
 def healthcheck():
-    response = app.response_class(
-            response=json.dumps({"result":"OK - healthy"}),
-            status=200,
-            mimetype='application/json'
-    )
+    response = app.response_class(response=json.dumps(
+        {"result": "OK - healthy"}),
+                                  status=200,
+                                  mimetype='application/json')
 
     app.logger.info('Status request successfull')
     return response
 
+
 @app.route('/metrics')
 def metrics():
-    response = app.response_class(
-            response=json.dumps({"status":"success","code":0,"data":{"UserCount":140,"UserCountActive":23}}),
-            status=200,
-            mimetype='application/json'
-    )
+    response = app.response_class(response=json.dumps({
+        "status": "success",
+        "code": 0,
+        "data": {
+            "UserCount": 140,
+            "UserCountActive": 23
+        }
+    }),
+                                  status=200,
+                                  mimetype='application/json')
 
     app.logger.info('Metrics request successfull')
     return response
+
 
 @app.route("/")
 def hello():
@@ -32,8 +39,9 @@ def hello():
 
     return "Hello World!"
 
+
 if __name__ == "__main__":
     ## stream logs to a file
-    logging.basicConfig(filename='app.log',level=logging.DEBUG)
-    
-    app.run(host='0.0.0.0')
+    logging.basicConfig(filename='app.log', level=logging.DEBUG)
+
+    app.run(host='0.0.0.0', debug=True, port=8080)
